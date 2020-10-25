667970a7e859d4421b7feab83ae3251dd5674858
teerawut-2541 <teerawut.char@gmail.com>
2020-10-25 22:34:48 +0700

    link

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -1,103 +1,89 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-
-import EmojiEventsIcon from '@material-ui/icons/EmojiEvents';
-import HomeIcon from '@material-ui/icons/Home';
-import KingBedIcon from '@material-ui/icons/KingBed';
-import KitchenIcon from '@material-ui/icons/Kitchen';
-import BathtubIcon from '@material-ui/icons/Bathtub';
-import LiveTvIcon from '@material-ui/icons/LiveTv';
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-
-
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import * as Icons from "@material-ui/icons";
+import { DataLink } from "./datalink";
 
 const useStyles = makeStyles((theme) => ({
-
-    menubar:{
-        fontFamily : 'Nunito',
-        textTransform:"uppercase",
-        height:'100%',
-        width:'230px',
-        background: '#343A40',
-        display:'flex',
+  menubar: {
+    fontFamily: "Nunito",
+    textTransform: "uppercase",
+    height: "100%",
+    width: "230px",
+    background: "#343A40",
+    display: "flex",
+  },
+  menubarBrand: {
+    color: "#fff",
+    fontSize: "1rem",
+    letterSpacing: "3px",
+    position: "absolute",
+    top: "20px",
+    left: "30px",
+    borderBottom: "1px solid white",
+    paddingBottom: "10px",
+  },
+  menubarHeader: {
+    position: "absolute",
+    top: "60px",
+    left: "-10px",
+  },
+  menubarUI: {
+    listStyle: "none",
+    lineHeight: "350%",
+  },
+  menubarlink: {
+    textDecoration: "none",
+    color: "#fff",
+    cursor: "pointer",
+    fontSize: "1rem",
+    letterSpacing: "2px",
+    "&:hover": {
+      color: "#51BCDA",
     },
-    menubarBrand:{
-        color:'#fff',
-        fontSize : '1rem',
-        letterSpacing :'3px',
-        position:'absolute',
-        top:'20px',
-        left : '30px',
-        borderBottom:'1px solid white',
-        paddingBottom: '10px'
-    },
-    menubarHeader:{
-        position:'absolute',
-        top:'60px',
-        left:'-10px'
-    },
-    menubarUI:{
-        listStyle: 'none',
-        lineHeight:'350%',
-    },
-    menubarlink:{
-        textDecoration: 'none',
-        color : '#fff',
-        cursor:'pointer',
-        fontSize : '1rem',
-        letterSpacing :'2px',
-        "&:hover":{
-            color:'#51BCDA'
-        }
-    },
-    menubarIcon:{
-        marginRight:'10px',
-        position:'relative',
-        top:'5px'
-    },
-    iconBack:{
-        color:'#fff',
-        padding:' 4px',
-        marginLeft: '200px',
-        position:'relative',
-        top:'8px',
-        cursor:'pointer'
-    }
-
+  },
+  menubarIcon: {
+    marginRight: "10px",
+    position: "relative",
+    top: "5px",
+  },
+  iconBack: {
+    color: "#fff",
+    padding: " 4px",
+    marginLeft: "200px",
+    position: "relative",
+    top: "8px",
+    cursor: "pointer",
+  },
 }));
 
 export default function Menubar(props) {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.menubar}>
-            <i onClick={props.handleDrawerClose}><ArrowBackIosIcon className={classes.iconBack}/></i>
-            <span className={classes.menubarBrand}><EmojiEventsIcon/> Smarthome  </span>
-            <div className={classes.menubarHeader}>
-                <ul className={classes.menubarUI}>
-                    <li>
-                        <Link to="/home" className={classes.menubarlink}><i className={classes.menubarIcon}><HomeIcon/></i>Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/bedroom" className={classes.menubarlink}><i className={classes.menubarIcon}><KingBedIcon/></i>Bedroom</Link>
-                    </li>
-                    <li>
-                        <Link to="/kitchen" className={classes.menubarlink}><i className={classes.menubarIcon}><KitchenIcon/></i>Kitchen</Link>
-                    </li>
-                    <li>
-                        <Link to="/bathroom" className={classes.menubarlink}><i className={classes.menubarIcon}><BathtubIcon/></i>Bathroom</Link>
-                    </li>
-                    <li>
-                        <Link to="/livingroom" className={classes.menubarlink}><i className={classes.menubarIcon}><LiveTvIcon/></i>living room</Link>
-                    </li>
-                    <li>
-                        <Link to="/account" className={classes.menubarlink}><i className={classes.menubarIcon}><LiveTvIcon/></i>living room</Link>
-                    </li>
-                </ul>
-                
-            </div>
-        </div>
-    )
+  return (
+    <div className={classes.menubar}>
+      <i onClick={props.handleDrawerClose}>
+        <Icons.ArrowBackIos className={classes.iconBack} />
+      </i>
+      <span className={classes.menubarBrand}>
+        <Icons.EmojiEvents /> Smarthome{" "}
+      </span>
+      <div className={classes.menubarHeader}>
+        <ul className={classes.menubarUI}>
+          {DataLink.map((item, index) => {
+            return (
+              <li key={index}>
+                <Link to={item.path} className={classes.menubarlink}>
+                  <i className={classes.menubarIcon}>
+                    {item.icons}
+                  </i>
+                  {item.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/datalink.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import * as Icons  from '@material-ui/icons';
+export const DataLink = [
+  {
+    title: "Home",
+    path: "/Home",
+    icons: <Icons.Home/>
+  },
+  {
+    title: "Bedroom",
+    path: "/bedroom",
+    icons: <Icons.KingBed/>
+  },
+  {
+    title: "Kitchen",
+    path: "/kitchen",
+    icons: <Icons.Kitchen/>
+  },
+  {
+    title: "Bathroom",
+    path: "/bathroom",
+    icons: <Icons.Bathtub/>
+  },
+  {
+    title: "living room",
+    path: "/livingroom",
+    icons: <Icons.LiveTv/>
+  },
+  {
+    title: "living room",
+    path: "/account",
+    icons: <Icons.LiveTv/>
+  },
+]
+
