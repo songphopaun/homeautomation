00d890ccda4a2a529ac60fc098c184219413ecf4
teerawut-2541 <teerawut.char@gmail.com>
2020-10-25 23:01:49 +0700

    g

---
+++ src/function/service.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+export function Fetchdht(){
+   let url='https://smarthome-bu.online/sensor_DHT11List';
+   var data=[]
+    axios.get(url)
+      .then(res => {
+        data.push(res.data)
+      })
+      return data
+}
+export function Fetchdhtlineone(){
+    let url='https://smarthome-bu.online/sensor_DHT11List';
+   var data=[]
+    axios.get(url)
+      .then(res => {
+        data.push(res.data)
+      })
+      return data
+}
